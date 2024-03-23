93b8b30527148b62deb541849560170af4679cdb
Mulaa <louismulaa06@gmail.com>
2024-03-23 15:16:47 +0300

    third lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=2;
+const  num2=31; 
+const multiply=num1 * num2 ;
+console.log(multiply)
 
-// Write your code here
+let random= Math.ceil(Math.random()*10);
+
+let num3= 12;
+let num4=8;
+let mod= num3 % num4;
+let max= Math.max(20);
